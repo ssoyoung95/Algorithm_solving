5b8abc5558f4613d29cb922ea09da66581f1dfd9
So young Han <ssoyoung95@hotmail.com>
2020-07-24 16:59:53 +0900

    solved practice problems of JUNGOL
    - Code no.116,118,119,117,526,527
    ;Added instructions of debugging problems.
    - Code no.119
    ;The problem I couldn't see a solution again as I expected.
    - Code no.117
    :changed wrong code no.177 to no.117
    ;I couldn't figure out what's wrong with the way I solved problem. again and again!!!!!!

--- C_Basic/119 디버깅 형성평가4.c
+++ C_Basic/119 디버깅 형성평가4.c
@@ -1,3 +1,28 @@
+﻿/*다음의 프로그램을 작성하고 디버깅을 하며 ①, ②, ③ 위치의 값을 watches에서 a의 값을 출력하시오.
+
+즉, ​①에서 a의 값,  ②에서 a의 값, ③에서 a의 값을 1,2,3과 바꾸어 출력하면 됩니다.
+
+ int main(void)
+
+{	
+	int a = 0;
+	time_t now;
+	struct tm tt; 
+	time(&now);
+	tt = *localtime(&now);------①
+	a = tt.t, _year;-------②
+	a += tt.tm_mon;
+	a += tt.tm_monday;
+	printf("%d %d %d\n", 1, 2, 3);-------③ 
+	//위 문장에서 출력될 값들을 각각① ② ③ 위치에서의 a의 값으로 바꾸어 준다.
+	return 0;
+
+
+
+}
+
+(PC의 시간이 맞는지 확인하세요. 시간이 다르면 결과가 틀릴 수 있습니다.)
+*/
 #include <stdio.h>
 
 int main(void)
@@ -6,7 +31,7 @@ int main(void)
 	int a = 0;
 	time_t now;
 	struct tm tt; 
-	//index���� ã�ƺôµ� struct�� ����ü��� ���ͼ� �о�ôµ� �����ϰ� �; ���� �������� �𸣰�����
+	//index에서 찾아봤는데 struct가 구조체라고 나와서 읽어봤는데 이해하고 싶어도 무슨 내용인지 모르갰어요ㅜ
 	time(&now);
 	tt = *localtime(&now);
 	a = tt.t, _year;
