4de8a917808e9ff2ca2e67c27d917ca8dc216220
Cheolho Kang <kbright0912@gmail.com>
2020-10-31 12:01:55 +0900

    Fixed problem 150

--- C_Basic/150_배열1_형성평가1.c
+++ C_Basic/150_배열1_형성평가1.c
@@ -1,21 +1,18 @@
 
 #include <stdio.h>
 
-	int main(void)
+int main(void)
+{
+	int idx;
+	char alph[10];
+	for (idx = 0; idx < 10; idx++)
 	{
-
-
-		int idx;
-		char alph[10];
-		for (idx = 0; idx < 10; idx++)
-		{
-			scanf("%c ", &alph[idx]);
-		}
-		for (idx = 9; 0 <= idx; idx--)
-		{
-			scanf("%c ", &alph[idx]);
-			printf("%c ", alph[idx]);
-		}
-
-		return 0;
+		scanf(" %c", &alph[idx]);
 	}
+	for (idx = 9; 0 <= idx; idx--)
+	{
+		printf("%c ", alph[idx]);
+	}
+
+	return 0;
+}
