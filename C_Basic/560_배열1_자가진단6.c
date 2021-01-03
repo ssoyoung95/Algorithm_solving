a4461264fcdb137852c520febccf4b88c17967b8
Cheolho Kang <kbright0912@gmail.com>
2021-01-03 12:21:58 +0900

    Solving 560

---
+++ C_Basic/560_배열1_자가진단6.c
@@ -0,0 +1,24 @@
+// http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=197&sca=1090
+
+#include <stdio.h>
+
+int main(void)
+{
+   int  array[10];
+   int idx;
+   //minimum? 1000을 넘지 않는다
+   int maximum = 1001;
+
+   for (idx = 0; idx < 10; idx++)
+   {
+      scanf("%d", &array[idx]);
+      
+      if (maximum>array[idx])
+      //가장 작은수   
+
+   }
+
+   printf(");
+
+   return 0;
+}
