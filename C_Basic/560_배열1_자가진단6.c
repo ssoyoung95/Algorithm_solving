9238590be670c15c751a85729f51a3d25b5ad8af
Cheolho Kang <kbright0912@gmail.com>
2021-01-03 12:33:39 +0900

    Answer of 560

--- C_Basic/560_배열1_자가진단6.c
+++ C_Basic/560_배열1_자가진단6.c
@@ -4,21 +4,30 @@
 
 int main(void)
 {
-   int  array[10];
-   int idx;
+   int array[10];
+
    //minimum? 1000을 넘지 않는다
    int maximum = 1001;
 
-   for (idx = 0; idx < 10; idx++)
+   for (int idx = 0; idx < 10; idx++)
    {
       scanf("%d", &array[idx]);
-      
-      if (maximum>array[idx])
-      //가장 작은수   
-
+    //   if (maximum>array[idx])
+    //   //가장 작은수
    }
 
-   printf(");
+   int result = 1001;
+
+    // 1. 배열을 탐색 (왼쪽에서 오른쪽으로)
+	for (int idx = 0; idx < 10; idx++)
+	{
+		// 2. 현재까지 탐색한 범위 내에서 가장 작은 수를 기록
+		if( result > array[idx])
+		{
+			result = array[idx];
+		}
+	}
+   printf("%d\n", result);
 
    return 0;
 }
